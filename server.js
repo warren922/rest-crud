999a9c8a2f6dbdbe223e583a7b4ba042166a208a
Warren Chan <warren.chan@ygn.org.hk>
2017-07-08 21:27:05 +0800

    changed for player

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express  = require('express'),
-    path     = require('path'),
-    bodyParser = require('body-parser'),
-    app = express(),
-    expressValidator = require('express-validator');
+var express = require('express'),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  app = express(),
+  expressValidator = require('express-validator');
 
 
 /*Set EJS template Engine*/
-app.set('views','./views');
-app.set('view engine','ejs');
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true })); //support x-www-form-urlencoded
@@ -15,23 +15,21 @@ app.use(bodyParser.json());
 app.use(expressValidator());
 
 /*MySql connection*/
-var connection  = require('express-myconnection'),
-    mysql = require('mysql');
+var connection = require('express-myconnection'),
+  mysql = require('mysql');
 
 app.use(
-
-    connection(mysql,{
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : 'test',
-        debug    : false //set true if you wanna see debug logger
-    },'request')
-
+  connection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: 'warren123',
+    database: 'warren_sunrise',
+    debug: false //set true if you wanna see debug logger
+  }, 'request')
 );
 
-app.get('/',function(req,res){
-    res.send('Welcome');
+app.get('/', function (req, res) {
+  res.send('Welcome');
 });
 
 
@@ -40,199 +38,165 @@ var router = express.Router();
 
 
 /*------------------------------------------------------
-*  This is router middleware,invoked everytime
-*  we hit url /api and anything after /api
-*  like /api/user , /api/user/7
-*  we can use this for doing validation,authetication
-*  for every route started with /api
---------------------------------------------------------*/
-router.use(function(req, res, next) {
-    console.log(req.method, req.url);
-    next();
+ *  This is router middleware,invoked everytime
+ *  we hit url /api and anything after /api
+ *  like /api/user , /api/user/7
+ *  we can use this for doing validation,authetication
+ *  for every route started with /api
+ --------------------------------------------------------*/
+router.use(function (req, res, next) {
+  console.log(req.method, req.url);
+  next();
 });
 
-var curut = router.route('/user');
-
+var curut = router.route('/players');
+const tblPlayer = 'tbl_player';
 
 //show the CRUD interface | GET
-curut.get(function(req,res,next){
-
-
-    req.getConnection(function(err,conn){
-
-        if (err) return next("Cannot Connect");
-
-        var query = conn.query('SELECT * FROM t_user',function(err,rows){
-
-            if(err){
-                console.log(err);
-                return next("Mysql error, check your query");
-            }
-
-            res.render('user',{title:"RESTful Crud Example",data:rows});
-
-         });
-
+curut.get(function (req, res, next) {
+  req.getConnection(function (err, conn) {
+    if (err) return next("Cannot Connect");
+    var query = conn.query('SELECT * FROM ' + tblPlayer, function (err, rows) {
+      if (err) {
+        console.log(err);
+        return next("Mysql error, check your query");
+      }
+      res.json(rows);
     });
-
+  });
 });
 //post data to DB | POST
-curut.post(function(req,res,next){
+curut.post(function (req, res, next) {
 
-    //validation
-    req.assert('name','Name is required').notEmpty();
-    req.assert('email','A valid email is required').isEmail();
-    req.assert('password','Enter a password 6 - 20').len(6,20);
+  //validation
+  req.assert('name', 'Name is required').notEmpty();
+  req.assert('nickname', 'Nick Name is required').notEmpty();
+  req.assert('position', 'Position is required').notEmpty();
+  req.assert('score', 'Score is required').notEmpty();
 
-    var errors = req.validationErrors();
-    if(errors){
-        res.status(422).json(errors);
-        return;
-    }
+  var errors = req.validationErrors();
+  if (errors) {
+    res.status(422).json(errors);
+    return;
+  }
 
-    //get data
-    var data = {
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-     };
+  //get data
+  var data = {
+    name: req.body.name,
+    nickname: req.body.nickname,
+    position: req.body.position,
+    score: req.body.score,
+  };
 
-    //inserting into mysql
-    req.getConnection(function (err, conn){
-
-        if (err) return next("Cannot Connect");
-
-        var query = conn.query("INSERT INTO t_user set ? ",data, function(err, rows){
-
-           if(err){
-                console.log(err);
-                return next("Mysql error, check your query");
-           }
-
-          res.sendStatus(200);
-
-        });
-
-     });
-
+  //inserting into mysql
+  req.getConnection(function (err, conn) {
+    if (err) return next("Cannot Connect");
+    var query = conn.query("INSERT INTO tbl_player set ? ", data, function (err, rows) {
+      if (err) {
+        console.log(err);
+        return next("Mysql error, check your query");
+      }
+      res.sendStatus(200);
+    });
+  });
 });
 
 
 //now for Single route (GET,DELETE,PUT)
-var curut2 = router.route('/user/:user_id');
+var curut2 = router.route('/player/:playerId');
 
 /*------------------------------------------------------
-route.all is extremely useful. you can use it to do
-stuffs for specific routes. for example you need to do
-a validation everytime route /api/user/:user_id it hit.
+ route.all is extremely useful. you can use it to do
+ stuffs for specific routes. for example you need to do
+ a validation everytime route /api/user/:user_id it hit.
 
-remove curut2.all() if you dont want it
-------------------------------------------------------*/
-curut2.all(function(req,res,next){
-    console.log("You need to smth about curut2 Route ? Do it here");
-    console.log(req.params);
-    next();
+ remove curut2.all() if you dont want it
+ ------------------------------------------------------*/
+curut2.all(function (req, res, next) {
+  // console.log("You need to smth about curut2 Route ? Do it here");
+  console.log(req.params);
+  next();
 });
 
 //get data to update
-curut2.get(function(req,res,next){
+curut2.get(function (req, res, next) {
+  var playerId = req.params.playerId;
 
-    var user_id = req.params.user_id;
-
-    req.getConnection(function(err,conn){
-
-        if (err) return next("Cannot Connect");
-
-        var query = conn.query("SELECT * FROM t_user WHERE user_id = ? ",[user_id],function(err,rows){
-
-            if(err){
-                console.log(err);
-                return next("Mysql error, check your query");
-            }
-
-            //if user not found
-            if(rows.length < 1)
-                return res.send("User Not found");
-
-            res.render('edit',{title:"Edit user",data:rows});
-        });
-
+  req.getConnection(function (err, conn) {
+    if (err) return next("Cannot Connect");
+    var query = conn.query('SELECT * FROM ' + tblPlayer + ' WHERE id = ? ', [playerId], function (err, rows) {
+      if (err) {
+        console.log(err);
+        return next("Mysql error, check your query");
+      }
+      //if user not found
+      if (rows.length < 1)
+        return res.send("Player Not found");
+      res.json(rows[0]);
     });
+
+  });
 
 });
 
 //update data
-curut2.put(function(req,res,next){
-    var user_id = req.params.user_id;
+curut2.put(function (req, res, next) {
+  var playerId = req.params.playerId;
 
-    //validation
-    req.assert('name','Name is required').notEmpty();
-    req.assert('email','A valid email is required').isEmail();
-    req.assert('password','Enter a password 6 - 20').len(6,20);
+  //validation
+  req.assert('name', 'Name is required').notEmpty();
+  req.assert('nickname', 'Nick Name is required').notEmpty();
+  req.assert('position', 'Position is required').notEmpty();
+  req.assert('score', 'Score is required').notEmpty();
 
-    var errors = req.validationErrors();
-    if(errors){
-        res.status(422).json(errors);
-        return;
-    }
+  var errors = req.validationErrors();
+  if (errors) {
+    res.status(422).json(errors);
+    return;
+  }
 
-    //get data
-    var data = {
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-     };
+  //get data
+  var data = {
+    name: req.body.name,
+    nickname: req.body.nickname,
+    position: req.body.position,
+    score: req.body.score,
+  };
 
-    //inserting into mysql
-    req.getConnection(function (err, conn){
-
-        if (err) return next("Cannot Connect");
-
-        var query = conn.query("UPDATE t_user set ? WHERE user_id = ? ",[data,user_id], function(err, rows){
-
-           if(err){
-                console.log(err);
-                return next("Mysql error, check your query");
-           }
-
-          res.sendStatus(200);
-
-        });
-
-     });
-
+  //inserting into mysql
+  req.getConnection(function (err, conn) {
+    if (err) return next("Cannot Connect");
+    var query = conn.query("UPDATE " + tblPlayer + " SET ? WHERE id = ? ", [data, playerId], function (err, rows) {
+      if (err) {
+        console.log(err);
+        return next("Mysql error, check your query");
+      }
+      res.sendStatus(200);
+    });
+  });
 });
 
 //delete data
-curut2.delete(function(req,res,next){
-
-    var user_id = req.params.user_id;
-
-     req.getConnection(function (err, conn) {
-
-        if (err) return next("Cannot Connect");
-
-        var query = conn.query("DELETE FROM t_user  WHERE user_id = ? ",[user_id], function(err, rows){
-
-             if(err){
-                console.log(err);
-                return next("Mysql error, check your query");
-             }
-
-             res.sendStatus(200);
-
-        });
-        //console.log(query.sql);
-
-     });
+curut2.delete(function (req, res, next) {
+  var playerId = req.params.playerId;
+  req.getConnection(function (err, conn) {
+    if (err) return next("Cannot Connect");
+    var query = conn.query("DELETE FROM " + tblPlayer + " WHERE id = ? ", [playerId], function (err, rows) {
+      if (err) {
+        console.log(err);
+        return next("Mysql error, check your query");
+      }
+      res.sendStatus(200);
+    });
+  });
 });
 
 //now we need to apply our router here
 app.use('/api', router);
 
 //start Server
-var server = app.listen(3000,function(){
+var server = app.listen(3000, function () {
 
-   console.log("Listening to port %s",server.address().port);
+  console.log("Listening to port %s", server.address().port);
 
 });
